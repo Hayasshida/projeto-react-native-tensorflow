193d6040b438844175e36069b2120d233e9872d0
Hayasshida <pedrohayashida9@gmail.com>
2024-06-05 12:35:09 -0300

    feat: i18n

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import { KeyboardAvoidingView, SafeAreaView, View } from 'react-native';
+import { KeyboardAvoidingView, SafeAreaView, View, FlatList } from 'react-native';
 import { useState } from 'react';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+import { StatusBar } from 'expo-status-bar';
 import { ActivityIndicator } from 'react-native-paper';
 import styles from './src/constants/styles';
 import toxicityTest from './src/lib/toxicity';
-import { StatusBar } from 'expo-status-bar';
 import ToxicityIndicator from './src/components/ToxicityIndicator';
 import StyledInputText from './src/components/StyledInputText';
 import StyledButton from './src/components/StyledButton';
-import { FlatList } from 'react-native';
-import { height } from './src/constants/measures';
+import { en, ptBr, es } from './src/constants/languages';
+
 
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [text, setText] = useState("")
+  const [locale, setLocale] = useState(Localization.getLocales());
+
+  i18n.fallbacks = true
+  i18n.translations = {en, ptBr, es}
+  i18n.Locales = locale
 
   const classify = async (text) => {
       const response = await toxicityTest(text)
@@ -29,7 +36,7 @@ export default function App() {
         <FlatList style={styles.list}></FlatList>
         <KeyboardAvoidingView enabled behavior="position">
           <StyledInputText placeholder="Digite aqui..." onChangeText={setText} value={text}/>
-          <StyledButton children="Verificar" onPress={() => {classify(text); setIsLoading(true)}}/>
+          <StyledButton children={i18n.t("button")} onPress={() => {classify(text); setIsLoading(true)}}/>
         </KeyboardAvoidingView>
     </SafeAreaView>
   );
---
+++ src/constants/languages.js
@@ -0,0 +1,19 @@
+const ptBr = {
+    title: "Verifique a agressividade de seu texto",
+    placeholder: "Digite aqui...",
+    button: "Verificar"
+}
+
+const en = {
+    title: "Verify the toxicity of your text",
+    placeholder: "Type here...",
+    button: "Verify"
+}
+
+const es = {
+    title: "Comprueba la agresividad de tu texto",
+    placeholder: "Digite aquí...",
+    button: "Verificar"
+}
+
+export {ptBr, en, es}
