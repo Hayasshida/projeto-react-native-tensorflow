5f668b219018a8b7aa53449cc41cbe08e2be0135
hayasshida <pedrohayashida9@gmail.com>
2024-06-05 12:45:13 -0300

    feat: historico-flatlist

--- App.js
+++ App.js
@@ -8,16 +8,19 @@ import ToxicityIndicator from './src/components/ToxicityIndicator';
 import StyledInputText from './src/components/StyledInputText';
 import StyledButton from './src/components/StyledButton';
 import { FlatList } from 'react-native';
+import Message from './src/components/Message';
 import { height } from './src/constants/measures';
 
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [text, setText] = useState("")
+  const [history, setHistory] = useState([])
 
   const classify = async (text) => {
       const response = await toxicityTest(text)
       setIsLoading(false)  
+      // const isToxic = response.some((predictions) => predictions.results[0].match)
       console.log(response)
   }; 
 
@@ -26,10 +29,16 @@ export default function App() {
         <StatusBar style='light'/>
         <ToxicityIndicator/>
         {isLoading && <View style={styles.load}><ActivityIndicator size="large" color="#99272d" /></View>}
-        <FlatList style={styles.list}></FlatList>
+        <FlatList style={styles.list}
+        data={history}
+        render={({message}) => <Message mensagem={message.mensagem}/>}
+        />
+           
+      
+
         <KeyboardAvoidingView enabled behavior="position">
           <StyledInputText placeholder="Digite aqui..." onChangeText={setText} value={text}/>
-          <StyledButton children="Verificar" onPress={() => {classify(text); setIsLoading(true)}}/>
+          <StyledButton children="Verificar" onPress={() => {classify(text); setIsLoading(true); setHistory(text)}}/>
         </KeyboardAvoidingView>
     </SafeAreaView>
   );
---
+++ src/components/Message/index.jsx
@@ -0,0 +1,7 @@
+const Message = ({mensagem}) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{mensagem}</Text>
+    </View>
+  );
+
+  export default Message;
